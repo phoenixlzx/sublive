7aed639e62935db075d77f92ec129388bbbae409
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-04-28 13:36:53 +0800

    tweak

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-
-
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -54,6 +52,7 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
 
 
 module.exports = app;
